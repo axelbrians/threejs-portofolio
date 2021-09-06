45486c0cafa6aea5628cb074a076d109253ce689
axelbrians <axelbrians@gmail.com>
2021-09-06 21:02:25 +0700

    add: typewriter animation on my name

--- src/script.js
+++ src/script.js
@@ -162,3 +162,58 @@ const tick = () =>
 }
 
 tick()
+
+
+// HTML Animation
+const newRow = document.createElement('br');
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Axel Briano\nSuherik", "Axel", "axelbrians"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    if(textArray[textArrayIndex].charAt(charIndex) === '\\') {
+        typedTextSpan.textContent += "\n";
+        charIndex++;
+    } else {
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    }
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    if (textArray[textArrayIndex].charAt(charIndex - 1) === "n") {
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-2);
+        charIndex--;
+    } else {
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    }
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
